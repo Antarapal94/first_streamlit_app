47bb6def824a65d8c1d184e2e45b99e07916f1dc
Antarapal94 <82706261+Antarapal94@users.noreply.github.com>
2022-06-27 22:48:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 #created by the main python file.
 import streamlit
 streamlit.title('MY Parents new healthy diner')
-streamlit.header('Breakfast menu')
-streamlit.text('omega3 & Blueberry Oatmeal')
-streamlit.text('kale, spinach &Rocket smothie')
-streamlit.text('Hard boiled Free-range egg')
+streamlit.header('🥝Breakfast menu')
+streamlit.text('🥝omega3 & Blueberry Oatmeal')
+streamlit.text('🥝kale, spinach &Rocket smothie')
+streamlit.text('🥝Hard boiled Free-range egg')
